f583d1b5de82ae15b8ec71155e0ed02a6d1b6fd1
hkyoo <hkyoo988@naver.com>
2024-03-12 17:06:03 +0900

    Feat: Pagination 추가

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import *
 from pymongo import MongoClient
+import math
 
 client = MongoClient('mongodb+srv://sparta:jungle@cluster0.sfuhxqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client.dbjungle
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    page = int(request.args.get('page', 1))
+    print(page)
+    limit = 6
+    offset = (page - 1) * limit
+    posts = list(db.posts.find({},{'_id':False}).skip(offset).limit(limit))
+    tot_count = list(db.posts.find({},{'_id':False}))
+    last_page_num = math.ceil(len(tot_count) / limit)
+    print(posts)
+    return render_template("index.html", posts=posts, page=page, zip=zip, last = last_page_num)
 
 @app.route('/signUp', methods=['POST'])
 def signUp():
