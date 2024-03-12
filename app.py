4e02a870d2eff78bf60564fb23ebf2b9ab496df2
hkyoo <hkyoo988@naver.com>
2024-03-12 21:31:38 +0900

    REFACTOR:base template add

--- app.py
+++ app.py
@@ -41,6 +41,10 @@ def idCheck():
       return '0'
    else:
       return '1'
+   
+@app.route('/post', methods=['GET'])
+def post():
+    return render_template("post.html")
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
